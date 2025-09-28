f05f9789f589fe61780a1c84bba7341f49a43c57
ashton <ashtondsouza192@gmail.com>
2025-09-28 17:42:01 +0530

    Added middleware functionalities to handle unauthorized user and redirected to auth , thus protecting routes . Also not letting logged in users see the auth page as they are already logged in (redirected to homepage) .Finally also added logout functionality via docs

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,58 +1,80 @@
-    "use client"
+"use client";
 
-    import Link from "next/link";
-    import { Button } from "../ui/button";
+import Link from "next/link";
+import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 
+function Header() {
+  const router = useRouter();
 
-    function Header() {
-        const router = useRouter();
-        
-        const navItems = [
-        {   
-            id : 1,
-            title: "Create Post",
-            href:'/post/create'
-        },{   
-            id : 2,
-            title: "About me",
-            href:'/about'
-        }
-        
-        ]    
-            
-        
-        return ( 
-            <header className="border-b bg-background sticky top-0 z-10 ">
-                <div className="container mx-auto px-4 h-16 flex items-center justify-between">
-                <div className="flex items-center gap-6  ">
-                    <Link href="/" className="font-bold text-xl">BlogApp </Link>
-    <nav className="hidden md:flex items-center gap-6">
+  const navItems = [
     {
-        navItems.map(
-            (items)=>(
-                <Link key={items.id} href={items.href}>{items.title}</Link>
-        )  
-        )
+      id: 1,
+      title: "Create Post",
+      href: "/post/create",
+    },
+    {
+      id: 2,
+      title: "About me",
+      href: "/about",
+    },
+  ];
+
+  const handleLogout = async () => {
+    try {
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess: () => {
+            router.push("/auth"); // redirect to login page
+          },
+        },
+      });
+      toast("logged out successfully ! ");
+    } catch (e) {
+      console.log("Error : ", e);
+      toast("error while logging out ");
     }
-    </nav>
-                </div>
-                <div className='flex items-center gap-4'>
-    <div className="hidden md:block">
-        {/* keep placeholder  for search  */}
-    </div>
-    {/* placeholder for theme toggle  */}
-    <div className="flex items-center gap-2">
-        <Button className="cursor-pointer"  onClick={()=>{router.push('/auth')}}>
-    Login/Singup
-        </Button>
+  };
 
-    </div>
-                </div>
-                </div>
+  return (
+    <header className="border-b bg-background sticky top-0 z-10 ">
+      <div className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <div className="flex items-center gap-6  ">
+          <Link href="/" className="font-bold text-xl">
+            BlogApp{" "}
+          </Link>
+          <nav className="hidden md:flex items-center gap-6">
+            {navItems.map((items) => (
+              <Link key={items.id} href={items.href}>
+                {items.title}
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="hidden md:block">
+            {/* keep placeholder  for search  */}
+          </div>
+          {/* placeholder for theme toggle  */}
+          <div className="flex items-center gap-2">
+            <Button
+              className="cursor-pointer"
+              onClick={() => {
+                router.push("/auth");
+              }}
+            >
+              Login/Singup
+            </Button>
+            <Button className="cursor-pointer" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
 
-            </header>
-        );
-    }
-
-    export default Header;
+export default Header;
---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+// logout button for logged in users !!
+
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+ 
+// define protected routes ...
+const protectedRoutes = ["/profile", "/post/create", "/post/edit"];
+
+// get the session from better auth docs ...
+export async function middleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+  const session = getSessionCookie(request);
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathName.startsWith(route)
+  );
+  if (isProtectedRoute && !session) {
+    // redirect user to auth page since user is not loggedin / authenticated
+    return NextResponse.redirect(new URL("/auth", request.url));
+    // toast message ???
+  }
+  // if user is already logged in and is accessingt the auth page /route
+  // redirect to homepage
+  if (pathName === "/auth" && session) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile/:path*", "/post/create", "/post/edit/:path*", "/auth"],
+};
