76e6387608ed1db8e921515356de692eb9abe970
ashton <ashtondsouza192@gmail.com>
2025-09-30 11:05:44 +0530

    Added the form to create posts , did error handling and rendering via  aria ...

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,9 +1,24 @@
+import PostForm from "@/components/post/post-form";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
 function CreatePost() {
-    return (  
-        <div>
-            Create Post     
+  return (
+    <div>
+      <main className="py-10">
+        <div className="max-w-4xl mx-auto">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-4xl font-bold">
+                Create New Post
+              </CardTitle>
+              <CardContent />
+              <PostForm />
+            </CardHeader>
+          </Card>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
 
 export default CreatePost;
---
+++ src/components/post/post-form.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import z from "zod";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTransition } from "react";
+
+function PostForm() {
+  const [isPending, startTransition] = useTransition();
+
+  // check posts db schema for elements ..
+  // post form schema for  validation
+  const postSchema = z.object({
+    title: z
+      .string()
+      .min(3, "Title should be atleast 3 characters long")
+      .max(255, "Title should not exceed 255 characters"),
+    description: z
+      .string()
+      .min(3, "Description should be atleast 3 characters long")
+      .max(255, "Description should not exceed 255 characters"),
+    content: z.string().min(3, "Content should be atleast 3 characters long"),
+  });
+  type PostFormValues = z.infer<typeof postSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostFormValues>({
+    resolver: zodResolver(postSchema),
+
+    defaultValues: {
+      title: "",
+      description: "",
+      content: "",
+    },
+  });
+  // handlesSubmit is  provided by react-hook-form !
+  const onFormSubmit = async (data: PostFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onFormSubmit)} className="space-y-6">
+        <div className="space-y-6">
+          <Label htmlFor="title">Title</Label>
+          <Input
+            id="title"
+            placeholder="Enter post title"
+            {...register("title")}
+            disabled={isPending}
+            aria-invalid={!!errors.title}
+            aria-describedby={errors.title ? "title-error" : undefined}
+          />
+          {errors.title?.message && (
+            <p id="title-error" className="mt-1 text-sm text-red-600">
+              {String(errors.title.message)}
+            </p>
+          )}
+        </div>{" "}
+        <div className="space-y-6">
+          <Label htmlFor="description">Description</Label>
+          <Textarea
+            id="description"
+            className="resize-none"
+            placeholder="Enter short  post description"
+            {...register("description")}
+            disabled={isPending}
+            aria-invalid={!!errors.description}
+            aria-describedby={
+              errors.description ? "description-error" : undefined
+            }
+          />
+          {errors.description?.message && (
+            <p id="description-error" className="mt-1 text-sm text-red-600">
+              {String(errors.description.message)}
+            </p>
+          )}
+        </div>
+        <div className="space-y-6">
+          <Label htmlFor="content">Content</Label>
+          <Textarea
+            id="content"
+            className="min-h-[250px] resize-none"
+            placeholder="Enter the content"
+            {...register("content")}
+            disabled={isPending}
+            aria-invalid={!!errors.content}
+            aria-describedby={errors.content ? "content-error" : undefined}
+          />
+          {errors.content?.message && (
+            <p id="content-error" className="mt-1 text-sm text-red-600">
+              {String(errors.content.message)}
+            </p>
+          )}
+        </div>
+        <Button type="submit" disabled={isPending} className="mt-5 w-full">
+          {isPending ? "Creating  Post ..." : "Create Post"}
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default PostForm;
